e7e327902de2bb4dc8b016a9dfabe765b0c5adbe
FelipeAlvarenga013 <96801416+FelipeAlvarenga013@users.noreply.github.com>
2025-03-04 15:00:13 -0500

    Initial commit

---
+++ js/codigo.js
@@ -0,0 +1,202 @@
+"use strict";
+
+//Chave da api
+const keyApi = 'api_key=fe926addb733eac566b4ba275de51a87';
+
+//Busca pelo ID
+const http1 = `https:api.themoviedb.org/3/movie/&append_to_response=videos&language=pt-BR`;
+
+//Buscar pelo Nome do filme
+const http2 = `https://api.themoviedb.org/3/search/movie?${keyApi}&language=pt-BR&query=carros 2`;
+
+//Funcão de buscar filmes pelo pesquisar;
+
+if(sessionStorage.getItem('ch')!=null) {
+    document.querySelector('#buscar').value=sessionStorage.getItem('ch');
+    document.querySelector('#buscar').focus();
+    document.querySelector('#artFilho1>h1').textContent=sessionStorage.getItem('ch').toUpperCase()+sessionStorage.getItem('ch').slice(1)+":";
+    //buscarFilmes.call(sessionStorage.getItem('ch'));
+    sessionStorage.clear();
+}
+
+document.querySelector('#buscar').addEventListener('input',function(){
+    
+    buscarFilmes.apply(this.value);
+    
+})
+
+//Buscar filmes pela barra de pesquisa;
+async function buscarFilmes() {
+    
+    try{
+        
+        if(this.length!=0) {
+            //Mudar nome da tela principal;
+            document.querySelector('#artFilho1>h1').textContent=this[0].toUpperCase()+this.slice(1)+":";
+            
+            //Apagar elementos existente;
+            
+            
+            const fetch1 = await fetch(`https://api.themoviedb.org/3/search/movie?${keyApi}&language=pt-BR&query= ${this}`);
+            const json1 = await fetch1.json();
+       
+            
+            criarElementos.call(json1);
+            
+        }else {
+            console.log('passou aqui');
+            //Volta com o nome(Melhores Filmes);
+            document.querySelector('#artFilho1>h1').textContent='Melhores Filmes:'
+            //Apagar elementos existente;
+            document.querySelectorAll('section>#secFilho1>article').forEach((vl)=>{
+                vl.remove();
+            });
+
+            const fetcApi1 = await fetch(`https://api.themoviedb.org/3/movie/popular?${keyApi}&language=pt-BR&page=1`);
+
+            const fetchJson1 = await fetcApi1.json();
+            
+            
+            //Função de criar elementos na pagina html;
+            
+            
+            criarElementos.call(fetchJson1) ;
+            
+        }
+    }catch{
+
+    }
+}
+
+
+//Função para chamar api dos filmes
+
+async function chamarApi() {
+    try{
+        
+        if(document.querySelector('#body').id==='body') {
+            const fetcApi1 = await fetch(`https://api.themoviedb.org/3/movie/popular?${keyApi}&language=pt-BR&page=1`);
+            const fetchJson1 = await fetcApi1.json();
+            
+            
+            //Função de criar elementos na pagina html;
+            criarElementos.call(fetchJson1) ;
+            
+        }
+        
+    }catch {
+        
+    }
+    
+} 
+//Executando função de cima;
+
+//Funcão de criar elementos;
+
+function criarElementos(){
+    //function do button de detalhes do filmes;
+    document.querySelectorAll('section>#secFilho1>article').forEach((vl,id,arr)=>{
+        vl.remove()
+        
+    });
+
+    for(let n1=0;n1<this.results.length;n1++) {
+        
+        //Elementos...
+        const but1 = document.createElement('button');
+        const h2 = document.createElement('h2');
+        const p1 = document.createElement('p');
+        const img1 = document.createElement('img');
+        const article1 = document.createElement('article');
+        const i1 = document.createElement('i');
+        const a1 = document.createElement('a');
+        
+        //Setando Nome dentro do buttão;
+        a1.textContent='Detalhes';
+        
+        
+        //Setando Nome dentro do Paragrafo;
+        p1.textContent=this.results[n1].vote_average.toFixed(1);
+        
+        //Setando Nome dentro do H2;
+        h2.textContent=this.results[n1].title;
+        
+        //Colocando um title quando passar por cima do elementos Article dos filmes/
+        article1.setAttribute('title',this.results[n1].title);
+        
+        //Colocando Elemento a(Link) dentro do elemento h2;
+        
+        //Class dos Elementos de cima..;
+        but1.setAttribute('class','but1');
+        article1.setAttribute('class','article1');
+        h2.setAttribute('class','h2Js');
+        p1.setAttribute('class','pJs');
+        i1.setAttribute('class','fa-solid fa-star');
+        a1.setAttribute('class','links');
+        
+        
+        //Setando a API de IMAGENS dentro do elemento IMG ;
+        img1.setAttribute('src',`https://image.tmdb.org/t/p/w500/${this.results[n1].poster_path}`);
+        
+        //Setando Elementos de dentro da pagina HTML Principal;
+        document.querySelector('#secFilho1').appendChild(article1);
+        document.querySelectorAll('.article1')[n1].appendChild(img1);
+        document.querySelectorAll('.article1')[n1].insertBefore(p1,document.querySelectorAll('.but1')[n1]);
+        document.querySelectorAll('.article1')[n1].insertBefore(h2,document.querySelectorAll('.pJs')[n1]);
+        document.querySelectorAll('.pJs')[n1].appendChild(i1);
+        document.querySelectorAll('.article1')[n1].appendChild(but1);
+        document.querySelectorAll('.but1')[n1].appendChild(a1);
+        console.log(this);
+        
+    }
+    
+    document.querySelectorAll('.but1>.links').forEach((vl,id)=>{
+        //loop com os elementos button e evento click;
+        vl.addEventListener('click',function(){
+            console.log(document.querySelectorAll('.but1')[id].parentElement.children[1].textContent)
+            localStorage.clear('api');
+            localStorage.setItem('api',document.querySelectorAll('.but1')[id].parentElement.children[1].textContent);
+           
+            vl.setAttribute('href','./index2.html')
+            
+            //fetcApi2(document.querySelectorAll('.but1')[id].parentElement.children[1].textContent);
+        })
+        
+    });
+    
+
+
+
+
+    //Buscando ID do filme com o nome do filme;
+    /* async function fetcApi2(element1) {
+     
+        const fetch1 = await fetch(`https://api.themoviedb.org/3/search/movie?${keyApi}&language=pt-BR&query=${element1}`);
+        const json1 = await fetch1.json();
+        //console.log(json1.results[0].id);
+        
+        pegaDetalhe(json1.results[0].id);
+    }
+    
+    
+    //Pegando Detalhe do filme com ID;
+    async function pegaDetalhe(id) {
+        
+        
+        const fetch1 = await fetch(`https:api.themoviedb.org/3/movie/${id}?${keyApi}&append_to_response=videos&language=pt-BR`);
+        const json1 = await fetch1.json();  
+    }
+     */
+    
+    
+}
+
+chamarApi();
+//export default chamarApi();
+
+
+
+
+
+
+
---
+++ js/codigo2.js
@@ -0,0 +1,133 @@
+"use strict";
+
+//import api from '../js/codigo.js'
+
+
+const n11 = document.querySelector('#buscar').addEventListener('input',function(){
+    
+    sessionStorage.clear();
+    sessionStorage.setItem('ch',this.value);
+    window.history.back();
+    this.focus();
+
+    
+})
+
+//Evento de barra de pesquisa, Assim que aperta;
+
+
+//Pegando dados da pagina principal;
+
+
+const keyApi = 'api_key=fe926addb733eac566b4ba275de51a87';
+
+//Busca pelo ID
+const http1 = `https:api.themoviedb.org/3/movie/&append_to_response=videos&language=pt-BR`;
+
+//Buscar pelo Nome do filme
+const http2 = `https://api.themoviedb.org/3/search/movie?${keyApi}&language=pt-BR&query=carros 2`;
+
+
+//Função assicrona
+
+async function buscaApi() {
+    //Buscando api do filmes
+    
+    //api buscada pelo nome do filme;
+    const fetc1 = await fetch(`https://api.themoviedb.org/3/search/movie?${keyApi}&language=pt-BR&query=${localStorage.getItem('api')}`);
+    //convertendo fetch em json();
+    const json1 = await fetc1.json();
+    
+    //api buscada pelo ID do filme
+    const fetc2 = await fetch(`https:api.themoviedb.org/3/movie/${json1.results[0].id}?${keyApi}&append_to_response=videos&language=pt-BR`);
+    //convertendo fetch em json();
+    const json2 = await fetc2.json();
+
+    createElement1.call(json2);
+    
+}
+
+buscaApi();
+
+//Função para criar elementos no HTML;
+function createElement1() {
+    const addElementos = document.querySelector('#artFilho1');
+    const addElementos2 = document.querySelector('#artFilho2');
+    
+    //Elementos para ser criados e adicionado na pagina;
+    const elementos = {
+        img1:document.createElement('img'),
+        h2:document.createElement('h2'),
+        i:document.createElement('i'),
+    }
+    //Adicionando foto;
+    elementos.img1.setAttribute('src',`https://image.tmdb.org/t/p/w500/${this.poster_path}`);
+    addElementos.appendChild(elementos.img1);
+    
+    //Adicionando h2 nome do filme;
+    elementos.h2.textContent=this.original_title;
+    addElementos.appendChild(elementos.h2);
+    
+    
+    //Addicionando elementos p(paragrafos) no article#artFilho1;
+    for(let n1 = 0;n1<3;n1++) {
+        const p1 = document.createElement('p')
+        addElementos.appendChild(p1);
+    };
+    //Primeiro Elemento p(Paragrafo);
+    //Adicionando voto de pontuação;
+    elementos.i.setAttribute('class','fa-solid fa-star');
+    elementos.i.textContent=this.vote_average.toFixed(1)
+    const paragrafos = document.querySelectorAll('#artFilho1>p');
+    paragrafos[0].setAttribute('id','strela');
+    paragrafos[0].appendChild(elementos.i)
+    
+    
+    //Segundo Elemento p(Paragrafo);
+    paragrafos[1].textContent=this.tagline;
+    
+    //Addicionando elementos p(paragrafos) no article#artFilho2;
+    for(let n1=0;n1<4;n1++) {
+        const p1 = document.createElement('p')
+        addElementos2.appendChild(p1);
+    }
+    
+    //Addicionando elemento i dentro do elemento p(paragrafo);
+    for(let n1=0;n1<4;n1++){
+        const elementosi = document.createElement('i');
+        document.querySelectorAll('#artFilho2>p')[n1].appendChild(elementosi);
+    }
+    
+    //Addicionando attributo ID para os primeiros 4 elemetos p(paragrafo) dentro do article#artFilho2;
+    document.querySelectorAll('#artFilho2>p')[0].setAttribute('id','p0');
+    document.querySelectorAll('#artFilho2>p')[1].setAttribute('id','p1');
+    document.querySelectorAll('#artFilho2>p')[2].setAttribute('id','p2');
+    document.querySelectorAll('#artFilho2>p')[3].setAttribute('id','p3');
+    
+    //Addicionando Emoji no elemento article#artFilho2
+    document.querySelectorAll('#artFilho2>p>i')[0].setAttribute('class','fa-solid fa-money-bill');
+    document.querySelectorAll('#artFilho2>p>i')[1].setAttribute('class','fa-solid fa-chart-line')
+    document.querySelectorAll('#artFilho2>p>i')[2].setAttribute('class','fa-solid fa-hourglass-start');
+    document.querySelectorAll('#artFilho2>p>i')[3].setAttribute('class','fa-solid fa-audio-description');
+    
+    //Adicionando mais elementos p(paragrafos);
+    for(let n1=1;n1<5;n1++) {
+        const paragrafoCriados = document.querySelector('#artFilho2');
+        
+        const elementoP = document.createElement('p');
+        
+        paragrafoCriados.insertBefore(elementoP,document.querySelector(`#p${n1}`));
+    }
+    
+    const paragrafoCriados = document.querySelectorAll('#artFilho2>p');
+    
+    paragrafoCriados[1].textContent=this.budget.toLocaleString('pt-BR',{style:'currency', currency:'BRL'});
+    
+    paragrafoCriados[3].textContent=this.revenue
+    .toLocaleString('pt-BR',{style:'currency', currency:'BRL'});
+    
+    paragrafoCriados[5].textContent=this.runtime+' minutos'
+    
+    paragrafoCriados[7].textContent=this.overview
+    
+}
